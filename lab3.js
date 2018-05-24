01d7afc47a0c20fba2b3d34d0ea610d4ea3a5373
ya-ya-it <ya-ya-it@yandex.com>
2018-05-24 16:34:01 -0400

    initial commit

---
+++ lab3.js
@@ -0,0 +1,114 @@
+const connect = require('connect');
+const url = require('url');
+
+const app = connect();
+
+//I think this is more interesting solution but I also wrote and comented the
+//solution that you asked in the lab
+
+function calculate(req, res, next) {
+  const queryString = url.parse(req.url, true).query;
+
+  const { x, y } =  queryString;
+
+  const operatorURL = url.parse(req.url, true).pathname;
+  var reply = '';
+
+  switch (operatorURL) {
+  case '/add':
+    var num = parseFloat(x) + parseFloat(y);
+    reply = ` ${x} + ${y} = ${num}`;
+    break;
+  case '/subtract':
+    var num = parseFloat(x) - parseFloat(y);
+    reply = ` ${x} - ${y} = ${num}`;
+    break;
+  case '/multiply':
+    var num = parseFloat(x) * parseFloat(y);
+    reply = ` ${x} * ${y} = ${num}`;
+    break;
+  case '/divide':
+    if (y != 0) {
+      var num = parseFloat(x) / parseFloat(y);
+      reply = ` ${x} / ${y} = ${num}`;
+    } else {
+      reply = 'Never divide by 0';
+    }
+    break;
+  default:
+    reply = 'Please, enter the correct operator';
+  }
+
+  res.end(reply);
+}
+
+function fallback(req, res, next) {
+  res.end('What?');
+  res.end();
+}
+
+app.use('/', calculate);
+app.use(fallback);
+
+app.listen(3000);
+
+//Lab solution
+
+// function add(req, res, next) {
+//   const queryString = url.parse(req.url, true).query;
+//
+//   const { x, y } = queryString;
+//
+//   const result = parseFloat(x) + parseFloat(y);
+//
+//   res.end(`${x} + ${y} = ${result}`);
+// }
+//
+// function subtract(req, res, next) {
+//   const queryString = url.parse(req.url, true).query;
+//
+//   const { x, y } = queryString;
+//
+//   const result = parseFloat(x) - parseFloat(y);
+//
+//   res.end(`${x} - ${y} = ${result}`);
+// }
+//
+// function multiply(req, res, next) {
+//   const queryString = url.parse(req.url, true).query;
+//
+//   const { x, y } = queryString;
+//
+//   const result = parseFloat(x) * parseFloat(y);
+//
+//   res.end(`${x} * ${y} = ${result}`);
+// }
+//
+// function divide(req, res, next) {
+//   const queryString = url.parse(req.url, true).query;
+//
+//   const { x, y } = queryString;
+//
+//   if (y != 0) {
+//     const result = parseFloat(x) / parseFloat(y);
+//   } else {
+//     const result = 0;
+//   }
+//
+//   res.end(`
+//     ${x} / ${y} = ${result}
+//     `);
+// }
+//
+// function fallback(req, res, next) {
+//   res.end('Please, enter the correct operator');
+//   res.end();
+// }
+//
+// app.use('/add', add);
+// app.use('/subtract', subtract);
+// app.use('/multiply', multiply);
+// app.use('/divide', divide);
+// app.use(fallback);
+//
+// app.listen(3000);
